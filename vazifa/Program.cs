b08a9e794b7f116cb414c2fad29d6445edb9e227
Hilola <Anazbekova@gmail.com>
2024-11-10 21:27:57 +0500

    1-versiya 10 likdan 2 likka o'tish dasturi

---
+++ vazifa/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vazifa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("10 lik sonni kiriting: ");
+            int s=int.Parse(Console.ReadLine());
+
+            string natija = "";
+
+            while (s > 0)
+            {
+                int q = s % 2;
+                natija = q + natija;
+                s /= 2;
+            }
+
+            Console.WriteLine("2 lik natijasi: "+ natija);
+            Console.ReadKey();
+        }
+    }
+}
