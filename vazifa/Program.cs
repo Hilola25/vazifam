d6b934289a653393bf6ded63af7101ea871264a8
Hilola <Anazbekova@gmail.com>
2024-11-10 21:32:35 +0500

    2-versiya 10 likdan 8 likka o'tish dasturi

--- vazifa/Program.cs
+++ vazifa/Program.cs
@@ -11,18 +11,18 @@ namespace vazifa
         static void Main(string[] args)
         {
             Console.Write("10 lik sonni kiriting: ");
-            int s=int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
 
             string natija = "";
 
             while (s > 0)
             {
-                int q = s % 2;
+                int q = s % 8;
                 natija = q + natija;
-                s /= 2;
+                s /= 8;
             }
 
-            Console.WriteLine("2 lik natijasi: "+ natija);
+            Console.WriteLine("8 lik natijasi: "+ natija);
             Console.ReadKey();
         }
     }
